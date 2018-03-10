23ecbde57dc56c58326f704a69278eaecb44d815
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 20:23:31 +0100

    add variables.go

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main(){ 
-	PrintValues()
+	PrintVariables()
 }
---
+++ variables.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+"fmt"
+
+)
+
+func PrintVariables(){
+    var a = "initial value"
+    var b, c int = 1, 2
+    var d = true
+    var e int
+    
+    fmt.Println(a,b,c,d,e)
+}
