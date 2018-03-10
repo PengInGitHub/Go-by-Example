b8639e9ecb648d033ecd3e70367772f6061ab664
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-10 20:10:12 +0100

    add values.go

---
+++ values.go
@@ -0,0 +1,19 @@
+package main 
+
+import (
+"fmt"
+
+)
+
+func PrintValues(){
+    
+    fmt.Println("go"+"lang")
+    fmt.Println("1+1=",1+1)
+    fmt.Println("7/3=",7.0/3.0)
+
+    fmt.Println(true&&false)
+    fmt.Println(true||false)
+    fmt.Println(!false)
+
+
+}
