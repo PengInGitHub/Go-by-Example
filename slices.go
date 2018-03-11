be75a129b2cc51b0c0e6a07dbb5aecd0fc92b91c
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-11 18:06:31 +0100

    use the builtin make() to create slice

--- slices.go
+++ slices.go
@@ -5,8 +5,14 @@ import (
 
 )
 
-func Print(){
+//slices are a key data type in Go, giving a more powerful interface to sequences than arrays
+
+//to create an empty slice with non-zero length, use the builtin make
+
+func PrintSlices(){
     
+    s := make([]string, 3)
+    fmt.Println("emp:", s)
+
     
-    fmt.Println()
 }
