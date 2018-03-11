68a0c8438b6f9bb140a56f240bd4290e70ec938c
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-11 17:03:36 +0100

    init errors.go

---
+++ errors.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+"fmt"
+"errors"
+)
+//In Go it’s idiomatic to communicate errors via an explicit, separate return value.
+
+//Go’s approach makes it easy to see which functions return errors and to handle them using the same language constructs employed for any other, non-error tasks.
+
+func PrintErrors(){
+    
+    
+    fmt.Println()
+}
