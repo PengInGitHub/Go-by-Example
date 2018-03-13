72e25596820723398dcecb0bf78f307642651366
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-13 07:57:05 +0100

    use index in range

--- range.go
+++ range.go
@@ -7,6 +7,18 @@ import (
 
 func PrintRange(){
     
-    
-    fmt.Println()
+//use range to sum the numbers in a slice. Arrays work like this too.
+    nums := []int{1,2,4,32,2}  
+    sum := 0 
+    for _,num := range nums{
+        sum += num
+    } 
+    fmt.Println(sum)
+
+//use index in range
+    for i,num := range nums{
+        if num==32 {
+           fmt.Println("index:",i) 
+        }
+    }
 }
