f2c1cdbc6e90fed12dd14a51549b46b250ddea7c
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-13 08:44:36 +0100

    add dvaradic function example: sum()

--- variadic-functions.go
+++ variadic-functions.go
@@ -6,8 +6,17 @@ import (
 )
 //Variadic functions can be called with any number of trailing arguments.
 //For example, fmt.Println is a common variadic function.
+
+func sum(nums ...int){
+    fmt.Print(nums, " ")
+    total := 0
+    for _,num := range nums{
+        total += num 
+    }
+    fmt.Print(total)
+}
+
+
 func PrintVariadicFunctions(){
-    
-    
-    fmt.Println()
+    sum(3,4,5)    
 }
