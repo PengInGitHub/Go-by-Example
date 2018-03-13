a6e6afe2bf43d4cfca1781cc15187504c4596674
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-13 08:38:47 +0100

    add definition of varadic functions

---
+++ variadic-functions.go
@@ -0,0 +1,13 @@
+package main 
+
+import (
+"fmt"
+
+)
+//Variadic functions can be called with any number of trailing arguments.
+//For example, fmt.Println is a common variadic function.
+func PrintVariadicFunctions(){
+    
+    
+    fmt.Println()
+}
