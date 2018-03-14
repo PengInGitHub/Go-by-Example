5889954b454613adc2d5bc832e4609406c90645c
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-14 07:31:46 +0100

    finish closures-anonymous functions

--- closures.go
+++ closures.go
@@ -7,8 +7,24 @@ import (
 //Go supports anonymous functions, which can form closures.
 //Anonymous functions are useful when you want to define a function inline without having to name it.
 
+
+//func returns another func
+//This function intSeq returns another function, which we define anonymously in the body of intSeq. 
+
+func intSeq() func() int{
+    i := 0
+    return func() int{
+        i++
+        return i
+    }
+
+}
+
+
 func PrintClosures(){
     
-    
-    fmt.Println()
+    nextInc := intSeq()
+    fmt.Println(nextInc())
+    fmt.Println(nextInc())
+    fmt.Println(nextInc())
 }
