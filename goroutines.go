ecb237e467d7a78dc18e46b91509d2126b43652e
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-15 07:16:08 +0100

    call a function in the usual way, running it synchronously.

--- goroutines.go
+++ goroutines.go
@@ -6,8 +6,15 @@ import (
 )
 
 //A goroutine is a lightweight thread of execution.
+//let programms run asynchronously
+
+func f(from string){
+    for i:=0;i<3;i++{
+        fmt.Println(from,":",i)
+    }
+}
+
 func PrintGoroutines(){
-    
-    
-    fmt.Println()
+    //call a function in the usual way, running it synchronously.
+    f("ohlala")
 }
