7e5cb8bf9487ae4cedf0f67b2f4c24f80f483a9f
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-15 07:29:37 +0100

    start goroutine for an anonymous function call

--- goroutines.go
+++ goroutines.go
@@ -17,4 +17,17 @@ func f(from string){
 func PrintGoroutines(){
     //call a function in the usual way, running it synchronously.
     f("ohlala")
+
+    //run the function cocurrently in a goroutine by using go func()
+    //this new goroutine will execute concurrently with the calling one.
+    go f("goroutine")
+
+    //start goroutine for an anonymous function call 
+    //now the two functions are running asynchronously
+
+    go func(msg string) {
+        fmt.Println(msg)
+    }("going")
+
+
 }
