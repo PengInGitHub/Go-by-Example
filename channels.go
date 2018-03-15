d1a93178b85322cadef6d4d50f5596e2e14793c0
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-15 07:48:55 +0100

    add usage of channels, which is as a pipe to deliver values from one goroutine to another goroutine

--- channels.go
+++ channels.go
@@ -6,6 +6,7 @@ import (
 )
 
 //Channels are the pipes that connect concurrent goroutines.
+//You can send values into channels from one goroutine and receive those values into another goroutine.
 
 func PrintChannels(){
     
