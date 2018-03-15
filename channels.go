19b3192e7a7f21f01afddf3d74ff19d09aa25304
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-15 07:52:16 +0100

    send a value into channel

--- channels.go
+++ channels.go
@@ -9,7 +9,9 @@ import (
 //You can send values into channels from one goroutine and receive those values into another goroutine.
 
 //create a new channel with make(chan val-type)
+//channels are typed by the value they convey
 
+//send a value into channel: channel <- value 
 func PrintChannels(){
     
     
