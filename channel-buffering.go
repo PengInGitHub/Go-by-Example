0e18fd03001233c734be20fddeed798623ded1bd
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-15 08:15:34 +0100

    add example of buffered channels

--- channel-buffering.go
+++ channel-buffering.go
@@ -7,8 +7,16 @@ import (
 
 //Buffered channels accept a limited number of values without a corresponding receiver for those values.
 
+//By default channels are unbuffered, meaning that they will only accept sends (chan <-) 
+//if there is a corresponding receive (<- chan) ready to receive the sent value. 
+
 func PrintChannelBuffering(){
     
-    
-    fmt.Println()
+    messages := make(chan string,2)
+    messages <- "buffered"
+    messages <- "channel"
+
+    fmt.Println(<-messages)
+    fmt.Println(<-messages)
+
 }
