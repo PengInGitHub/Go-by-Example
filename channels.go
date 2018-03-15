a8eaa68bb142771f7d726192095c8c8eab6b0561
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-15 07:51:04 +0100

    create a new channel

--- channels.go
+++ channels.go
@@ -8,6 +8,8 @@ import (
 //Channels are the pipes that connect concurrent goroutines.
 //You can send values into channels from one goroutine and receive those values into another goroutine.
 
+//create a new channel with make(chan val-type)
+
 func PrintChannels(){
     
     
