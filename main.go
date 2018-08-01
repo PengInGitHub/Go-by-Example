1ed350f6b15405be144dfe21c13de8cfb094175e
PengInGitHub <felixdeutschland@hotmail.com>
2018-08-01 14:10:07 +0200

    add flad exercise

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
-package main 
+package main
 
-func main(){ 
-	PrintChannelBuffering()
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	flags := playFlag()
+	fmt.Printf(flags)
+}
+
+func playFlag() string {
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of question:answer")
+	startDate := flag.String("start", "2018-07-20", "start date for analysis")
+	testID := flag.Int("test", 3, "ID of test model")
+	sslDisabled := flag.Bool("ssl", true, "if SSL is disabled")
+
+	var flagVar int
+	flag.IntVar(&flagVar, "referenceID", 1, "ID of reference ID")
+	flag.Parse()
+	return fmt.Sprintf("csv has name: %s, analysis start date: %s, ID of test: %d, SSL disabled: %t, reference: %d\n", *csvFileName, *startDate, *testID, *sslDisabled, flagVar)
 }
